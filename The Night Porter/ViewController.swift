a6556f9ed14e49fcf7e011d9518bc3cf7be3141f
Tiago Lopes de Paiva Dionysio da Fonseca <tiagoapolo@gmail.com>
2017-05-04 00:00:51 -0300

    Initial Commit

---
+++ The Night Porter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Night Porter
+//
+//  Created by Tiago Lopes de Paiva Dionysio da Fonseca on 04/05/17.
+//  Copyright © 2017 Apolox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
